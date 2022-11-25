18b5622ab464e8a6093563795fca7852bf69a6ec
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2022-11-25 01:26:53 +0100

    Add the line type

--- src/custom_types.py
+++ src/custom_types.py
@@ -1,7 +1,11 @@
 """Module for custom typing support for the other modules."""
 
 from collections.abc import Callable
+from dataclasses import dataclass
+from numbers import Number
+from typing import Tuple
 
+import numpy as np
 from nptyping import Float, NDArray, Shape
 
 Vector = NDArray[Shape["Size"], Float]
@@ -10,3 +14,48 @@ Matrix = NDArray[Shape["Size, Size"], Float]
 Function = Callable[[Vector], float]
 Gradient = Callable[[Vector], Vector]
 Hessian = Callable[[Vector], Matrix]
+
+
+@dataclass
+class Line:
+    """Class to define a line.
+
+    Args:
+        x0 (Vector): The starting point.
+        direction (Vector): The direction of the line.
+        t_range (Tuple[Number, Number]): The range around the point.
+        n (int, optional): The number of point to sample.
+            Can be specified in the following format functions.
+            Defaults to 1000.
+    """
+
+    x0: Vector
+    direction: Vector
+    t_range: Tuple[Number, Number]
+    n: int = 1000
+
+    def ts(self, n=None) -> NDArray[Shape["Var"], Float]:
+        """Generate the ts of the line.
+
+        Args:
+            n (int, optional): The number of points to generate.
+                Defaults to self.n.
+
+        Returns:
+            NDArray[Shape["n"], Float]: Array containing the ts.
+        """
+        return np.linspace(*self.t_range, num=n or self.n)
+
+    def xs(self, n=None) -> NDArray[Shape["Var"], Float]:
+        """Generate the points of the line.
+
+        Args:
+            n (int, optional): The number of points to generate.
+                Defaults to self.n.
+
+        Returns:
+            NDArray[Shape["Size, n"], Float]: Array containing the points of the line.
+        """
+        ts = self.ts(n=n or self.n)
+        xs = self.x0.reshape(-1, 1) + ts * self.direction.reshape(-1, 1)
+        return xs
