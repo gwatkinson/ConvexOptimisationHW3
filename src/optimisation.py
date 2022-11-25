f36485326650f10b4088d3268bd470bff8be78dc
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2022-11-25 01:26:35 +0100

    Start the unconstrained optimisation class

--- src/optimisation.py
+++ src/optimisation.py
@@ -1,1 +1,9 @@
 """Module that provides optimization functions."""
+
+
+class UnconstrainedOptimizer:
+    """Unconstrained optimizer."""
+
+    def __init__(self):
+        """Initiate a new instance."""
+        pass
