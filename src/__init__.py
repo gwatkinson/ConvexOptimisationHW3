634547e99ea6286e4a1cd5ae30101016b9383e1a
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2022-11-26 23:17:23 +0100

    Add matplotlib params

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,19 @@
 
 In the context of the Convex Optimization course of the MVA
 """
+import matplotlib.pyplot as plt
+
+plt.style.use("seaborn")
+plt.rcParams["text.latex.preamble"] = r"\boldmath"
+
+SMALL_SIZE = 10
+MEDIUM_SIZE = 12
+BIGGER_SIZE = 15
+
+plt.rc("font", size=SMALL_SIZE)  # controls default text sizes
+plt.rc("axes", titlesize=MEDIUM_SIZE)  # fontsize of the axes title
+plt.rc("axes", labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+plt.rc("xtick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
+plt.rc("figure", titlesize=BIGGER_SIZE)  # fontsize of the figure title>
