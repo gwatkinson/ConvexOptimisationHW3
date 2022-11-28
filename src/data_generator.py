4c6db4fbeff12a0c9b7333b7bbc785f11b40b8e7
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2022-11-28 03:04:00 +0100

    Add a function to generate the data

---
+++ src/data_generator.py
@@ -0,0 +1,18 @@
+"""Module to generate pseudo data."""
+
+import numpy as np
+
+def generate_data(N=20, D=50, ld=10, sigma=5, density=0.2):
+    "Generates data matrix X and observations Y."
+    np.random.seed(1)
+    beta_star = np.random.randn(D)
+    idxs = np.random.choice(range(D), int((1-density)*D), replace=False)
+    for idx in idxs:
+        beta_star[idx] = 0
+    X = np.random.randn(N,D)
+    y = X.dot(beta_star) + np.random.normal(0, sigma, size=N)
+    Q = 0.5*np.identity(N)
+    p = y
+    A = np.vstack((X.T, -X.T))
+    b = ld * np.ones(2*D)
+    return X, y, beta_star, Q, p, A, b
